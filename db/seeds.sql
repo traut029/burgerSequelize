8d441e1f367956d9a0f828e00746e9e185de2b9a
Eric Trautmiller <trautmillereahs@gmail.com>
2018-04-14 15:59:50 -0500

    test

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
