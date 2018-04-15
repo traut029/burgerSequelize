1553d2be5f92a7cba6f4d99f57b17dccd7ffb51a
Eric Trautmiller <trautmillereahs@gmail.com>
2018-04-14 23:27:25 -0500

    back end basically done

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,29 +18,21 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+  burger.create(req.body.burgerName, function(result) {
+    // send back results
+    res.json(result);
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update(
-    {
-      sleepy: req.body.sleepy
-    },
-    condition,
-    function(result) {
+router.put("/api/devour", function(req, res) {
+  burger.update(req.body.burgerId,function(result) {
       if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       }
+      res.json(result)
       res.status(200).end();
-
+    
     }
   );
 });
