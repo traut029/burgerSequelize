e42a395a586e6366f4d08972ef8c611d2ce239d5
Eric Trautmiller <trautmillereahs@gmail.com>
2018-04-16 00:46:36 -0500

    minor

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,9 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id int AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255) NOT NULL,
     devoured boolean NOT NULL DEFAULT 0,
+    createdAt TIMESTAMP NOT NULL default current_timestamp,
 	PRIMARY KEY (id)
 );
