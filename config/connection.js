d0abb8c3f107080c424a54e5e6d10521fba92287
Eric Trautmiller <trautmillereahs@gmail.com>
2018-04-16 00:03:16 -0500

    heroku commit prep

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 8889,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_url) {
+    connection=mysql.createConnection(process.env.JAWSDB_url);
+}
+else {
+    connection = mysql.createConnection({
+        port: 8889,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
