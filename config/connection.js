f298ba6d54db328db90e7a808626c6d6b9cf91af
Eric Trautmiller <trautmillereahs@gmail.com>
2018-04-17 22:29:30 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var Sequelize = require("sequelize");
 var mysql=require("mysql");
 var sequelize;
 if (process.env.JAWSDB_URL) {
-    sequelize=mysql.createsequelize(process.env.JAWSDB_URL);
+    sequelize=mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
     var sequelize = new Sequelize("burgers_db", "root", "root", {
