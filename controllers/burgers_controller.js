fde2d8821ff496a74d4d24889e136b1eed5ab40b
Eric Trautmiller <trautmillereahs@gmail.com>
2018-04-17 22:16:25 -0500

    basic level functional

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,8 +7,8 @@ var router = express.Router();
 var Burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  Burger.findAll({}).then(function(data) {
+router.get("/", function (req, res) {
+  Burger.findAll({}).then(function (data) {
     var hbsObject = {
       burgers: data
     };
@@ -17,30 +17,33 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-    console.log(req.body)
-    console.log(req.body.burgerName)
+router.post("/api/burgers", function (req, res) {
+  console.log(req.body)
+  console.log(req.body.burgerName)
   Burger.create({
     burger_name: req.body.burgerName,
-  
-  }).then(function(result) {
+
+  }).then(function (result) {
     // send back results
     res.json(result);
   });
 });
 
-router.put("/api/devour", function(req, res) {
-  burger.update(req.body.burgerId,function(result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.json(result)
-      res.status(200).end();
-    
-    }
-  );
-});
+router.put("/api/devour", function (req, res) {
+  console.log("put ran")
+  console.log(req.body)
+  console.log(req.body.id)
+    Burger.update({
+      devoured: 1
+    }, {
+        where: {
+          id: req.body.id
+        }
+      }).then(function (result) {
+        res.json(result);
+      })
 
+  
+})
 // Export routes for server.js to use.
 module.exports = router;
